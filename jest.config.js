bf0233faeb60787e69c3814873a95296e1501965
Ben Halverson <benhalverson33@gmail.com>
2023-11-09 15:57:29 -0800

    added test coverage

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true, // Enable code coverage collection
+  coverageDirectory: 'coverage', // Specify the directory where coverage reports will be stored
 };
