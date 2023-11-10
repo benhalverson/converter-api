f6f50861bd550b972ef91b5352d0e35127aa5506
Ben Halverson <benhalverson33@gmail.com>
2023-11-09 22:01:18 -0800

    refactor and improve tests

--- src/utils/__tests__/tempature.test.ts
+++ src/utils/__tests__/tempature.test.ts
@@ -1,71 +1,93 @@
-import { convertTemperature } from '../tempature';
+import { convertTemperature } from "../tempature";
 
-describe('convertTemperature', () => {
-  it('should convert fahrenheit to celsius', () => {
-    expect(convertTemperature(32, 'fahrenheit', 'celsius')).toBe(0);
-    expect(convertTemperature(68, 'fahrenheit', 'celsius')).toBe(20);
-    expect(convertTemperature(212, 'fahrenheit', 'celsius')).toBe(100);
-  });
 
-  it('should convert celsius to fahrenheit', () => {
-    expect(convertTemperature(0, 'celsius', 'fahrenheit')).toBe(32);
-    expect(convertTemperature(20, 'celsius', 'fahrenheit')).toBe(68);
-    expect(convertTemperature(100, 'celsius', 'fahrenheit')).toBe(212);
-  });
+describe("convertTemperature", () => {
+	it("should convert fahrenheit to celsius", () => {
+		expect(convertTemperature(32, "fahrenheit", "celsius")).toBeCloseTo(0.00);
+		expect(convertTemperature(68, "fahrenheit", "celsius")).toBeCloseTo(20);
+		expect(convertTemperature(212, "fahrenheit", "celsius")).toBe(100);
+		expect(convertTemperature(-459.67, "fahrenheit", "celsius")).toBeCloseTo(-273.15);
 
-  it('should convert celsius to kelvin', () => {
-    expect(convertTemperature(0, 'celsius', 'kelvin')).toBe(273.15);
-    expect(convertTemperature(20, 'celsius', 'kelvin')).toBe(293.15);
-    expect(convertTemperature(100, 'celsius', 'kelvin')).toBe(373.15);
-  });
+	});
 
-  it('should convert kelvin to celsius', () => {
-    expect(convertTemperature(273.15, 'kelvin', 'celsius')).toBe(0);
-    expect(convertTemperature(293.15, 'kelvin', 'celsius')).toBe(20);
-    expect(convertTemperature(373.15, 'kelvin', 'celsius')).toBe(100);
-    expect(convertTemperature(-19, 'kelvin', 'celsius')).toBe(-292.15);
-  });
+	it("should convert celsius to fahrenheit", () => {
+		expect(convertTemperature(0, "celsius", "fahrenheit")).toBe(32);
+		expect(convertTemperature(20, "celsius", "fahrenheit")).toBe(68);
+		expect(convertTemperature(100, "celsius", "fahrenheit")).toBe(212);
+		expect(convertTemperature(-273.15, "celsius", "fahrenheit")).toBeCloseTo(-459.67);
+	});
 
-  it('should convert fahrenheit to kelvin', () => {
-    expect(convertTemperature(32, 'fahrenheit', 'kelvin')).toBe(273.15);
-    expect(convertTemperature(68, 'fahrenheit', 'kelvin')).toBe(293.15);
-    expect(convertTemperature(212, 'fahrenheit', 'kelvin')).toBe(373.15);
-  });
+	it("should convert celsius to kelvin", () => {
+		expect(convertTemperature(0, "celsius", "kelvin")).toBe(273.15);
+		expect(convertTemperature(20, "celsius", "kelvin")).toBe(293.15);
+		expect(convertTemperature(100, "celsius", "kelvin")).toBe(373.15);
+		expect(convertTemperature(-273.15, "celsius", "kelvin")).toBe(0);
+	});
 
-  it('should convert kelvin to fahrenheit', () => {
-    expect(convertTemperature(273.15, 'kelvin', 'fahrenheit')).toBe(32);
-    expect(convertTemperature(293.15, 'kelvin', 'fahrenheit')).toBe(68);
-    expect(convertTemperature(373.15, 'kelvin', 'fahrenheit')).toBeCloseTo(212);
-  });
+	it("should convert kelvin to celsius", () => {
+		expect(convertTemperature(273.15, "kelvin", "celsius")).toBe(0);
+		expect(convertTemperature(293.15, "kelvin", "celsius")).toBe(20);
+		expect(convertTemperature(373.15, "kelvin", "celsius")).toBe(100);
+		expect(convertTemperature(-19, "kelvin", "celsius")).toBe(-292.15);
+		expect(convertTemperature(0, "kelvin", "celsius")).toBe(-273.15);
+	});
 
-  it('should convert rankine to kelvin', () => {
-    expect(convertTemperature(0, 'rankine', 'kelvin')).toBe(0);
-    expect(convertTemperature(459.67, 'rankine', 'kelvin')).toBeCloseTo(255.37);
-    expect(convertTemperature(100, 'rankine', 'kelvin')).toBeCloseTo(55.56);
-    expect(convertTemperature(671.67, 'rankine', 'kelvin')).toBeCloseTo(373.15);
-  });
+	it("should convert fahrenheit to kelvin", () => {
+		expect(convertTemperature(32, "fahrenheit", "kelvin")).toBe(273.15);
+		expect(convertTemperature(68, "fahrenheit", "kelvin")).toBe(293.15);
+		expect(convertTemperature(212, "fahrenheit", "kelvin")).toBe(373.15);
+		expect(convertTemperature(-459.67, "fahrenheit", "kelvin")).toBe(0);
+	});
 
-  it('should convert kelvin to rankine', () => {
-    expect(convertTemperature(0, 'kelvin', 'rankine')).toBe(0);
-    expect(convertTemperature(255.37, 'kelvin', 'rankine')).toBeCloseTo(459.67);
-    expect(convertTemperature(372.03, 'kelvin', 'rankine')).toBeCloseTo(669.65);
+	it("should convert kelvin to fahrenheit", () => {
+		expect(convertTemperature(273.15, "kelvin", "fahrenheit")).toBe(32);
+		expect(convertTemperature(293.15, "kelvin", "fahrenheit")).toBe(68);
+		expect(convertTemperature(373.15, "kelvin", "fahrenheit")).toBeCloseTo(212);
+		expect(convertTemperature(0, "kelvin", "fahrenheit")).toBeCloseTo(-459.67);
+		expect(convertTemperature(-100, "kelvin", "fahrenheit")).toBeCloseTo(-639.67);
+	});
+
+	it("should convert rankine to kelvin", () => {
+		expect(convertTemperature(0, "rankine", "kelvin")).toBe(0);
+		expect(convertTemperature(459.67, "rankine", "kelvin")).toBeCloseTo(255.37);
+		expect(convertTemperature(100, "rankine", "kelvin")).toBeCloseTo(55.56);
+		expect(convertTemperature(671.67, "rankine", "kelvin")).toBeCloseTo(373.15);
+	});
+
+	it("should convert kelvin to rankine", () => {
+		expect(convertTemperature(0, "kelvin", "rankine")).toBe(0);
+		expect(convertTemperature(255.37, "kelvin", "rankine")).toBeCloseTo(459.67);
+		expect(convertTemperature(372.03, "kelvin", "rankine")).toBeCloseTo(669.65);
     
-  });
+	});
 
-  it('should convert rankine to fahrenheit', () => {
-    expect(convertTemperature(0, 'rankine', 'fahrenheit')).toBe(-459.67);
-    expect(convertTemperature(459.67, 'rankine', 'fahrenheit')).toBe(0);
-    expect(convertTemperature(671.67, 'rankine', 'fahrenheit')).toBeCloseTo(212);
-  });
+	it("should convert rankine to fahrenheit", () => {
+		expect(convertTemperature(0, "rankine", "fahrenheit")).toBe(-459.67);
+		expect(convertTemperature(459.67, "rankine", "fahrenheit")).toBe(0);
+		expect(convertTemperature(671.67, "rankine", "fahrenheit")).toBeCloseTo(212);
+	});
 
-  it('should convert fahrenheit to rankine', () => {
-    expect(convertTemperature(-459.67, 'fahrenheit', 'rankine')).toBe(0);
-    expect(convertTemperature(0, 'fahrenheit', 'rankine')).toBe(459.67);
-    expect(convertTemperature(212, 'fahrenheit', 'rankine')).toBeCloseTo(671.67);
-    expect(convertTemperature(84.2, 'fahrenheit', 'rankine')).toBeCloseTo(543.87);
-  });
+	it("should convert fahrenheit to rankine", () => {
+		expect(convertTemperature(-459.67, "fahrenheit", "rankine")).toBe(0);
+		expect(convertTemperature(0, "fahrenheit", "rankine")).toBe(459.67);
+		expect(convertTemperature(212, "fahrenheit", "rankine")).toBeCloseTo(671.67);
+		expect(convertTemperature(84.2, "fahrenheit", "rankine")).toBeCloseTo(543.87);
+	});
 
-  it.skip('should throw an error for invalid input', () => {
-    expect(() => convertTemperature(0, 'invalid', 'unit')).toThrow('Invalid input');
-  });
+	it("should throw an error for invalid input", () => {
+		expect(convertTemperature(0, "invalid", "invalid")).toBe(NaN);
+	});
+
+	it("should handle upper bounds", () => {
+		expect(convertTemperature(50000000, "fahrenheit", "celsius")).toBeCloseTo(27777760);
+	});
+
+	it("should handle lower bounds", () => {
+		expect(convertTemperature(-500, "fahrenheit", "celsius")).toBeCloseTo(-295.56);
+	});
+
+	it("should handle absolute zero (Kelvin)", () => {
+		expect(convertTemperature(0, "kelvin", "celsius")).toBe(-273.15);
+	});
+
 });
--- src/utils/tempature.ts
+++ src/utils/tempature.ts
@@ -1,37 +1,35 @@
+import { BigNumber } from "bignumber.js";
+
 /**
  * Utility function to convert temperature from one unit of measure to another
- * @param inputValue numberical value
+ * @param inputValue numerical value
  * @param fromUnit starting unit of measure
  * @param toUnit converted unit of measure
  * @returns value converted to the target unit of measure
  */
 export function convertTemperature(inputValue: number, fromUnit: string, toUnit: string): number {
-  switch(`${fromUnit}-${toUnit}`) {
-    case 'fahrenheit-celsius':
-      const fahrenheitToCelsius: number = (inputValue - 32) * 5 / 9;
-      return fahrenheitToCelsius;
-    case 'celsius-fahrenheit':
-      return (inputValue * 9 / 5) + 32;
-    case 'celsius-kelvin':
-      return inputValue + 273.15;
-    case 'kelvin-celsius':
-      return inputValue - 273.15;
-    case 'fahrenheit-kelvin':
-      return (inputValue - 32) * 5 / 9 + 273.15;
-    case 'kelvin-fahrenheit':
-      const kelvinToFahrenheit = (inputValue - 273.15) * 9 / 5 + 32;
-      return Math.round((kelvinToFahrenheit + Number.EPSILON) * 100) / 100;
-    case 'rankine-kelvin':
-      const rankineToKelvin: number = inputValue * 5 / 9;
-      return  Math.round((rankineToKelvin + Number.EPSILON) * 100) / 100;
-    case 'kelvin-rankine':
-      const kelvinToRankine: number = inputValue * 9 / 5;
-      return Math.round((kelvinToRankine + Number.EPSILON) * 100) / 100;
-    case 'rankine-fahrenheit':
-      return inputValue - 459.67;
-    case 'fahrenheit-rankine':
-      return inputValue + 459.67;
-    default:
-      return NaN;
-  }
+	const bigInputValue = new BigNumber(inputValue);
+
+	const conversions: { [key: string]: (value: BigNumber) => BigNumber } = {
+		"fahrenheit-celsius": (value) => value.minus(32).times(5).dividedBy(9),
+		"celsius-fahrenheit": (value) => value.times(9).dividedBy(5).plus(32),
+		"celsius-kelvin": (value) => value.plus(273.15),
+		"kelvin-celsius": (value) => value.minus(273.15),
+		"fahrenheit-kelvin": (value) => value.minus(32).times(5).dividedBy(9).plus(273.15),
+		"kelvin-fahrenheit": (value) => value.minus(273.15).times(9).dividedBy(5).plus(32),
+		"rankine-kelvin": (value) => value.times(5).dividedBy(9),
+		"kelvin-rankine": (value) => value.times(9).dividedBy(5),
+		"rankine-fahrenheit": (value) => value.minus(459.67),
+		"fahrenheit-rankine": (value) => value.plus(459.67),
+	};
+
+	const conversionKey = `${fromUnit}-${toUnit}`;
+	const conversionFunction = conversions[conversionKey];
+
+	if (conversionFunction) {
+		const result = conversionFunction(bigInputValue);
+		return result.toNumber(); 
+	} else {
+		return NaN;
+	}
 }
